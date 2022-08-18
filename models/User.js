30729ee93912d03f430424f9f7e75fe7c4d7cfce
LindsayLew <lmschwehr@gmail.com>
2022-08-17 19:00:46 -0700

    remember how to do mongoose foreign keys

--- models/User.js
+++ models/User.js
@@ -15,7 +15,7 @@ const userSchema = new User (
             required: true,
             unique: true,
             validate: {
-                // think I'm gonna need to use a custom validator function, found the regex pattern from regex libraries
+                // think I'm gonna need to use a custom validator function, found the regex pattern from regex libraries. .test tests for a match
                 validator: function(v) {
                     return /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/.test(v);
                 },
@@ -24,7 +24,13 @@ const userSchema = new User (
         }
     },
     {
-        
+        thoughts: {
+
+        }
     }
 
-)
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
