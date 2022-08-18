53b80955835b5d75d1179f8802bbb07e98beafeb
LindsayLew <lmschwehr@gmail.com>
2022-08-18 09:33:34 -0700

    index file for api routes

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -8,3 +8,15 @@ const {
     updateUser,
 } = require('../../controllers/userController');
 
+// /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// api/user/:userId
+
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
