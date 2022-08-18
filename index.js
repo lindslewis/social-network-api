59ec0671f327c63e9379352335a297afb0f322f7
LindsayLew <lmschwehr@gmail.com>
2022-08-17 18:32:28 -0700

    initial index file created

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const cwd = process.cwd();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+db.once('open', () => {
+    app.listen(PORT, () => {
+        console.log(`API server is running on port ${PORT}!`);
+    });
+});
