33b632f2982de11ed8e6e58afa6c806b08a57cb3
LindsayLew <lmschwehr@gmail.com>
2022-08-17 20:57:49 -0700

    added create user

---
+++ controllers/userController.js
@@ -0,0 +1,31 @@
+const { User, Thought } = require('../models');
+
+module.exports = {
+    // find all users
+    getUsers(req, res) {
+        User.find()
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+    // find singular user
+    getSingleUser(req, res) {
+        User.findOne({ _id: req.params.id})
+            .select('username')
+            .then((user) => 
+            !user
+                ? res.status(404).json({ message: 'No user with that ID'})
+                : res.json(user)
+            )
+            .catch((err) => res.status(500).json(err));
+    },
+    // create new user
+    createUser(req, res) {
+        User.create(req.body)
+            .then((user) => res.json(user))
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+
+}
