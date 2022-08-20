9ac1b7d5f3b39fe18f962e4b3860d4beebc3db5d
LindsayLew <lmschwehr@gmail.com>
2022-08-20 16:34:58 -0700

    showing up again but not with thoughts still

--- controllers/userController.js
+++ controllers/userController.js
@@ -8,19 +8,32 @@ module.exports = {
             .catch((err) => res.status(500).json(err));
     },
     // find singular user
+    // getSingleUser(req, res) {
+    //     User.findOne({ _id: req.params.userId})
+    //         .select('-__v')
+    //         .then(async (user) => 
+    //         !user
+    //             ? res.status(404).json({ message: 'No user with that ID'})
+    //             : res.json({
+    //                 user,
+    //                 thought: await Thought(req.params.userId),
+    //             })    
+    //         )
+    //         .catch((err) => {
+    //             console.log(err);
+    //             return res.status(500).json(err);
+    //         });
+    // },
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId})
             .select('-__v')
-            .then(async (user) => 
+            .then((user) => 
             !user
                 ? res.status(404).json({ message: 'No user with that ID'})
-                : res.json({
-                    user,
-                    thought: await thought(req.params.userId)
-                })
-                   
+                : res.json(user)    
             )
-            .catch((err) => res.status(500).json(err));
+            .catch((err) => res.status(500).json(err)
+           );
     },
     // create new user
     createUser(req, res) {
